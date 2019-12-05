73baaa84c394b983af25247b4aa779a26b35e005
Frederick Akalin <akalin@gmail.com>
2019-12-05 03:05:31 -0800

    Rejigger ip arithmetic

--- day05.py
+++ day05.py
@@ -2,14 +2,21 @@ def run_prog(program, input):
     memory = program[:]
 
     ip = 0
-    np = 0
+    input_pos = 0
     output = []
+    modes = 0
+    nargs = 0
 
     def compute_mode(i):
-        return (memory[ip] // (10**(i+2))) % 10
+        return (modes // (10**i)) % 10
+
+    def check_i(i):
+        if i >= nargs:
+            raise Exception(f'i={i} >= nargs={nargs}')
 
     def getp(i):
-        p = memory[ip+i+1]
+        check_i(i)
+        p = memory[ip+i]
         mode = compute_mode(i)
         if mode == 0:
             # position mode
@@ -20,64 +27,74 @@ def run_prog(program, input):
         raise Exception(f'Unknown mode {mode}')
 
     def setp(i, v):
+        check_i(i)
         mode = compute_mode(i)
         if mode != 0:
             raise Exception(f'Unexpected mode {mode} for setp')
-        p = memory[ip+i+1]
+        p = memory[ip+i]
         memory[p] = v
 
-    # np = parameter count
-    def adv(np):
+    def adv():
         nonlocal ip
-        ip += np + 1
+        ip += nargs
 
     while True:
         opcode = memory[ip] % 100
+        modes = memory[ip] // 100
+        ip += 1
 
         if opcode == 1:
             # Add
+            nargs = 3
             setp(2, getp(0) + getp(1))
-            adv(3)
+            adv()
 
         elif opcode == 2:
             # Multiply
+            nargs = 3
             setp(2, getp(0) * getp(1))
-            adv(3)
+            adv()
 
         elif opcode == 3:
             # Consume input
-            setp(0, input[np])
-            np += 1
-            adv(1)
+            nargs = 1
+            setp(0, input[input_pos])
+            input_pos += 1
+            adv()
 
         elif opcode == 4:
             # Produce output
+            nargs = 1
             output.append(getp(0))
-            adv(1)
+            adv()
 
         elif opcode == 5:
             # jne
+            nargs = 2
             if getp(0) != 0:
                 ip = getp(1)
             else:
-                adv(2)
+                adv()
 
         elif opcode == 6:
             # jeq
+            nargs = 2
             if getp(0) == 0:
                 ip = getp(1)
             else:
-                adv(2)
+                adv()
 
         elif opcode == 7:
             # lt
+            nargs = 3
             setp(2, int(getp(0) < getp(1)))
-            adv(3)
+            adv()
 
         elif opcode == 8:
             # eq
+            nargs = 3
             setp(2, int(getp(0) == getp(1)))
-            adv(3)
+            adv()
 
         elif opcode == 99:
             # halt
