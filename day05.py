e5cf2cfae5b36c43bb1c9f1f28689c4804390ad9
Frederick Akalin <akalin@gmail.com>
2019-12-04 23:36:44 -0800

    More refactoring

--- day05.py
+++ day05.py
@@ -4,57 +4,50 @@ def run_prog(program, input):
     ip = 0
     np = 0
     output = []
+
+    def getp(i):
+        p = memory[ip+i+1]
+        mode = (memory[ip] // (10**(i+2))) % 10
+        if mode == 0:
+            return memory[p]
+        if mode == 1:
+            return p
+        raise Exception(f'Unknown mode {modes[i]}')
+
+    def setp(i, v):
+        p = memory[ip+i+1]
+        memory[p] = v
+
     while True:
-        def get_param(i):
-            p = memory[ip+i+1]
-            mode = (memory[ip] // (10**(i+2))) % 10
-            if mode == 0:
-                return memory[p]
-            if mode == 1:
-                return p
-            raise Exception(f'Unknown mode {modes[i]}')
-
         opcode = memory[ip] % 100
-
         if opcode == 1:
-            p3 = memory[ip+3]
-            memory[p3] = get_param(0) + get_param(1)
+            setp(2, getp(0) + getp(1))
             ip += 4
         elif opcode == 2:
-            p3 = memory[ip+3]
-            memory[p3] = get_param(0) * get_param(1)
+            setp(2, getp(0) * getp(1))
             ip += 4
         elif opcode == 3:
-            p1 = memory[ip+1]
-            memory[p1] = input[np]
+            setp(0, input[np])
             np += 1
             ip += 2
         elif opcode == 4:
-            output.append(get_param(0))
+            output.append(getp(0))
             ip += 2
         elif opcode == 5:
-            if get_param(0) != 0:
-                ip = get_param(1)
+            if getp(0) != 0:
+                ip = getp(1)
             else:
                 ip += 3
         elif opcode == 6:
-            if get_param(0) == 0:
-                ip = get_param(1)
+            if getp(0) == 0:
+                ip = getp(1)
             else:
                 ip += 3
         elif opcode == 7:
-            p3 = memory[ip+3]
-            if get_param(0) < get_param(1):
-                memory[p3] = 1
-            else:
-                memory[p3] = 0
+            setp(2, int(getp(0) < getp(1)))
             ip += 4
         elif opcode == 8:
-            p3 = memory[ip+3]
-            if get_param(0) == get_param(1):
-                memory[p3] = 1
-            else:
-                memory[p3] = 0
+            setp(2, int(getp(0) == getp(1)))
             ip += 4
         elif opcode == 99:
             break
