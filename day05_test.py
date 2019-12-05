b87c1c2104219c692251322ee168ec7327829f02
Frederick Akalin <akalin@gmail.com>
2019-12-05 00:40:58 -0800

    More tests

--- day05_test.py
+++ day05_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from day05 import run_prog
+from itertools import chain
 
 class TestDay04(unittest.TestCase):
     def test_io(self):
@@ -15,6 +16,48 @@ class TestDay04(unittest.TestCase):
     def test_negative(self):
         memory, _ = run_prog([1101,100,-1,4,0], [])
         self.assertEqual(memory, [1101,100,-1,4,99])
+
+    def test_eq8_pos(self):
+        prog = [3,9,8,9,10,9,4,9,99,-1,8]
+
+        _, output = run_prog(prog, [8])
+        self.assertEqual(output, [1])
+
+        for x in chain(range(-100, 8), range(9, 100)):
+            _, output = run_prog(prog, [x])
+            self.assertEqual(output, [0])
+
+    def test_le8_pos(self):
+        prog = [3,9,7,9,10,9,4,9,99,-1,8]
+
+        for x in range(-100, 8):
+            _, output = run_prog(prog, [x])
+            self.assertEqual(output, [1])
+
+        for x in range(8, 100):
+            _, output = run_prog(prog, [x])
+            self.assertEqual(output, [0])
+
+    def test_eq8_imm(self):
+        prog = [3,3,1108,-1,8,3,4,3,99]
+
+        _, output = run_prog(prog, [8])
+        self.assertEqual(output, [1])
+
+        for x in chain(range(-100, 8), range(9, 100)):
+            _, output = run_prog(prog, [x])
+            self.assertEqual(output, [0])
+
+    def test_le8_imm(self):
+        prog = [3,3,1107,-1,8,3,4,3,99]
+
+        for x in range(-100, 8):
+            _, output = run_prog(prog, [x])
+            self.assertEqual(output, [1])
+
+        for x in range(8, 100):
+            _, output = run_prog(prog, [x])
+            self.assertEqual(output, [0])
 
 if __name__ == '__main__':
     unittest.main()
