f8e307c4e0db10fc2b49547312d8e2c95d0a4a02
Frederick Akalin <akalin@gmail.com>
2019-12-07 21:46:43 -0800

    More golfing

--- day08.py
+++ day08.py
@@ -1,18 +1,12 @@
 def chunked(l, n):
     return [l[i*n:(i+1)*n] for i in range(len(l) // n)]
 
-def get_pixel(layers, i):
-    for layer in layers:
-        if layer[i] != '2':
-            return layer[i]
-    raise Exception(f'couldn\'t find non-transparent pixel at pos {i}')
-
 def compute_day08(input, m, n):
     layers = chunked(input, m*n)
     min_layer = min(layers, key=lambda l: l.count('0'))
     part1 = min_layer.count('1') * min_layer.count('2')
 
-    image = [get_pixel(layers, i) for i in range(m*n)]
+    image = [next(x for x in p if x != '2') for p in zip(*layers)]
     rows = chunked(image, m)
     part2 = '\n'.join([''.join(r).replace('0', ' ').replace('1', '⋅') for r in rows])
     return part1, part2
