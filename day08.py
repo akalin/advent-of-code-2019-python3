1c58268c257c7ef2fa40244271051d7d206a5ba8
Frederick Akalin <akalin@gmail.com>
2019-12-07 21:22:17 -0800

    Some cleanup

--- day08.py
+++ day08.py
@@ -1,8 +1,5 @@
 import itertools
 
-def count_digits(layer, d):
-    return sum([1 for x in layer if x == d])
-
 def get_pixel(layers, i):
     for layer in layers:
         if layer[i] != '2':
@@ -11,21 +8,15 @@ def get_pixel(layers, i):
 
 def compute_day08(input, m, n):
     layers = [input[i*m*n:(i+1)*m*n] for i in range(len(input)//(m*n))]
-    min_i = -1
-    min_count = m*n
-    for i, l in enumerate(layers):
-        c = count_digits(l, '0')
-        if c < min_count:
-            min_i = i
-            min_count = c
-    p1 = count_digits(layers[min_i], '1') * count_digits(layers[min_i], '2')
+    min_layer = min(layers, key=lambda l: l.count('0'))
+    part1 = min_layer.count('1') * min_layer.count('2')
 
     image = [get_pixel(layers, i) for i in range(m*n)]
-    rows = [''.join(image[i*n:(i+1)*n]) for i in range(len(image)//m)]
-    return rows
+    rows = [''.join(image[i*m:(i+1)*m]) for i in range(len(image)//m)]
+    return part1, '\n'.join(rows)
 
 if __name__ == '__main__':
     with open('day08.input', 'r') as input_file:
         input = input_file.read()
-        output = compute_day08(input, 25, 6)
-        print(f'output: {output}')
+        p1, p2 = compute_day08(input, 25, 6)
+        print(f'part 1: {p1}, part 2:\n{p2}')
