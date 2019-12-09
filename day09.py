7ce693fc9bcddc396bb47b1c6a0c18ef6216af2d
Frederick Akalin <akalin@gmail.com>
2019-12-08 21:09:56 -0800

    Day 9 solution

---
+++ day09.py
@@ -0,0 +1,161 @@
+import collections
+import itertools
+
+class Intputer(object):
+    def __init__(self, program):
+        self.memory = collections.defaultdict(int)
+        for i, x in enumerate(program):
+            self.memory[i] = x
+        self.ip = 0
+        self.rel_base = 0
+        self.waiting_for_input = False
+        self.halted = False
+
+    def run(self, input, output):
+        if self.halted:
+            raise Exception('Called run when halted')
+
+        if self.waiting_for_input and len(input) == 0:
+            raise Exception('Called run with empty input while waiting for input')
+
+        modes = 0
+        nargs = 0
+
+        def compute_mode(i):
+            return (modes // (10**i)) % 10
+
+        def check_i(i):
+            if i >= nargs:
+                raise Exception(f'i={i} >= nargs={nargs}')
+
+        def getp(i):
+            check_i(i)
+            p = self.memory[self.ip+i]
+            mode = compute_mode(i)
+            if mode == 0:
+                # position mode
+                return self.memory[p]
+            if mode == 1:
+                # immediate mode
+                return p
+            if mode == 2:
+                # rel base mode
+                return self.memory[self.rel_base + p]
+            raise Exception(f'Unknown mode {mode}')
+
+        def setp(i, v):
+            check_i(i)
+            mode = compute_mode(i)
+            p = self.memory[self.ip+i]
+            if mode == 0:
+                # position mode
+                self.memory[p] = v
+                return
+            if mode == 2:
+                # rel base mode
+                self.memory[self.rel_base + p] = v
+                return
+
+            raise Exception(f'Unexpected mode {mode} for setp')
+
+        def adv():
+            self.ip += nargs
+
+        while True:
+            modes_opcode = self.memory[self.ip]
+            opcode = modes_opcode % 100
+            modes = modes_opcode // 100
+
+            self.ip += 1
+
+            if opcode == 1:
+                # Add
+                nargs = 3
+                setp(2, getp(0) + getp(1))
+                adv()
+
+            elif opcode == 2:
+                # Multiply
+                nargs = 3
+                setp(2, getp(0) * getp(1))
+                adv()
+
+            elif opcode == 3:
+                # Consume input
+                nargs = 1
+                if len(input) == 0:
+                    self.waiting_for_input = True
+                    self.ip -= 1
+                    return
+                self.waiting_for_input = False
+                v = input.pop(0)
+                setp(0, v)
+                adv()
+
+            elif opcode == 4:
+                # Produce output
+                nargs = 1
+                v = getp(0)
+                output.append(v)
+                adv()
+
+            elif opcode == 5:
+                # jne
+                nargs = 2
+                if getp(0) != 0:
+                    self.ip = getp(1)
+                else:
+                    adv()
+
+            elif opcode == 6:
+                # jeq
+                nargs = 2
+                if getp(0) == 0:
+                    self.ip = getp(1)
+                else:
+                    adv()
+
+            elif opcode == 7:
+                # lt
+                nargs = 3
+                setp(2, int(getp(0) < getp(1)))
+                adv()
+
+            elif opcode == 8:
+                # eq
+                nargs = 3
+                setp(2, int(getp(0) == getp(1)))
+                adv()
+
+            elif opcode == 9:
+                # relbase adjust
+                nargs = 1
+                self.rel_base += getp(0)
+                adv()
+
+            elif opcode == 99:
+                # halt
+                self.halted = True
+                return
+
+            else:
+                raise Exception(f'Unknown opcode {opcode}')
+
+def run_serial_mode(program):
+    intputer = Intputer(program)
+    output = []
+    intputer.run([2], output)
+    return output
+
+def compute_day09(input):
+    program = [int(x) for x in input.split(',')]
+    #program = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    #program = [1102,34915192,34915192,7,4,7,99,0]
+    #program =[104,1125899906842624,99]
+    return run_serial_mode(program)
+
+if __name__ == '__main__':
+    with open('day09.input', 'r') as input_file:
+        input = input_file.read()
+        output = compute_day09(input)
+        print(f'output: {output}')
