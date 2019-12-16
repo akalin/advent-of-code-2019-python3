0ae6c9887714837b7bfd916b9fa3fe4cb4b46833
Frederick Akalin <akalin@gmail.com>
2019-12-16 12:25:37 -0800

    part 1 tests

--- day16_test.py
+++ day16_test.py
@@ -8,5 +8,16 @@ class TestDay16(unittest.TestCase):
         nums_out = apply_fft(nums_in, 4)
         self.assertEqual(to_str(nums_out), '01029498')
 
+    def test_fft(self):
+        test_cases = [
+            ('80871224585914546619083218645595', '24176176'),
+            ('19617804207202209144916044189917', '73745418'),
+            ('69317163492948606335995924319873', '52432133'),
+        ]
+        for input, expected_out in test_cases:
+            nums_in = parse_input(input)
+            nums_out = apply_fft(nums_in, 100)
+            self.assertEqual(to_str(nums_out[:8]), expected_out)
+
 if __name__ == '__main__':
     unittest.main()
