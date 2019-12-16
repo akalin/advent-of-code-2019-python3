2b3cba30a09656753013a24ecda13396e7d3b583
Frederick Akalin <akalin@gmail.com>
2019-12-16 01:31:33 -0800

    rough version

--- day16.py
+++ day16.py
@@ -36,12 +36,38 @@ def apply_fft(nums_in, rounds):
 #        print(nums_out)
     return nums_out
 
+def compute_ith_second_half(nums_in, i):
+    s = sum(nums_in[i:]) % 10
+    return s
+
+def do_round_second_half(nums_in):
+    nums_out = [compute_ith_second_half(nums_in, i) for i in range(len(nums_in))]
+#    print(nums_in, nums_out)
+    return nums_out
+
+def apply_fft_second_half(nums_in, rounds):
+    nums_out = nums_in
+    for i in range(rounds):
+        nums_out = do_round_second_half(nums_out)
+    return nums_out
+
+def to_str(digits):
+    return ''.join([str(x) for x in digits])
+
 def compute_day16(input):
-#    input = '80871224585914546619083218645595'
+#    input = '12345678'
+    input = '80871224585914546619083218645595'
+
     nums_in = [int(x) for x in input.strip()]
     output = apply_fft(nums_in, 100)
-    output_s = ''.join([str(x) for x in output[:8]])
-    return output_s, None
+    part1 = to_str(output[:8])
+
+    h = len(nums_in)//2
+    output_second_half = apply_fft_second_half(nums_in[h:], 100)
+    if output_second_half != output[h:]:
+        raise
+
+    return part1, to_str(output_second_half)
 
 if __name__ == '__main__':
     with open('day16.input', 'r') as input_file:
