fe6b03c8899392aa19f7168fdff412844f14637b
Frederick Akalin <akalin@gmail.com>
2019-12-23 01:28:29 -0600

    rem unused code

--- day22.py
+++ day22.py
@@ -84,69 +84,6 @@ class Shuffle(object):
         shuffles = [Shuffle.parse(n, line) for line in input.strip().split('\n')]
         return prod(reversed(shuffles), Shuffle(n))
 
-# 7*(i + c)
-# 7*i + 0
-# -7*(i - 1)
-class Deck(object):
-    def __init__(self, count):
-        self.count = count
-        self.factor = 1
-        self.offset = 0
-
-    def get(self, i):
-        j = (self.factor*i + self.offset) % self.count
-        return j
-
-    def deal_new(self):
-        self.offset = self.get(-1)
-        self.factor = (self.factor * -1) % self.count
-#        print('new', self.factor, self.offset)
-
-    def cut(self, n):
-        self.offset = self.get(n)
-#        print('cut', self.factor, self.offset)
-
-    def deal_increment(self, n):
-        n_inv = modinv(n, self.count)
-        self.factor = (self.factor * n_inv) % self.count
-#        print('inc', self.factor, self.offset)
-
-    def to_list(self):
-        l = [0] * self.count
-        for i in range(self.count):
-            l[i] = self.get(i)
-        return l
-
-    def do_shuffle(self, lines):
-        for line in lines:
-            if len(line) == 0:
-                continue
-            words = line.split(' ')
-            if words[0] == 'cut':
-                n = int(words[1])
-                self.cut(n)
-            elif words[0] == 'deal' and words[1] == 'with':
-                n = int(words[3])
-                self.deal_increment(n)
-            elif words[0] == 'deal' and words[1] == 'into':
-                self.deal_new()
-
-    def __mul__(self, other):
-        # c(ax + b) + d = acx + cb + d
-        a = self.factor
-        b = self.offset
-        c = other.factor
-        d = other.offset
-        if self.count != other.count:
-            raise
-        new_deck = Deck(self.count)
-        new_deck.factor = (a * c) % self.count
-        new_deck.offset = (c*b + d) % self.count
-        return new_deck
-
-    def __str__(self):
-        return f'Deck(count={self.count}, factor={self.factor}, offset={self.offset})'
-
 def compute_day22(input):
     n1 = 10007
     sh1 = Shuffle.parse_multiple(n1, input)
