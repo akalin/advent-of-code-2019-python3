4bfecb49380e2570154bbb88624a90d92bc82b79
Frederick Akalin <akalin@gmail.com>
2019-12-24 04:52:23 -0600

    no need for gens

--- day15.py
+++ day15.py
@@ -35,7 +35,7 @@ def compute_day15(input):
         if n in walls:
             return []
         possible_neighbors = [n + d.vec() for d in all_directions]
-        return (m for m in possible_neighbors if (m not in walls))
+        return [m for m in possible_neighbors if (m not in walls)]
 
     G = nx.Graph()
 
