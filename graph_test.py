3bf7ede40f1248043276e897d06edd37c272c6b9
Frederick Akalin <akalin@gmail.com>
2019-12-25 01:30:48 -0600

    add 1 test

---
+++ graph_test.py
@@ -0,0 +1,15 @@
+from graph import *
+import networkx as nx
+import unittest
+
+class TestGraph(unittest.TestCase):
+    def test_bfs_edges(self):
+        G = nx.Graph()
+        G.add_edges_from([(0, 1), (1, 2), (1, 3), (2, 4), (3, 4)])
+
+        expected_edges = [(0, 1), (1, 2), (1, 3), (2, 4)]
+        edges = list(bfs_edges(source=0, successors=G.neighbors))
+        self.assertEqual(edges, expected_edges)
+
+if __name__ == '__main__':
+    unittest.main()
