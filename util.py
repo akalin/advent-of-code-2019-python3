abc28c16a60bd322d5a21404de2ef32e2d2f2b76
Frederick Akalin <akalin@gmail.com>
2019-12-25 01:20:25 -0600

    rename fn

--- util.py
+++ util.py
@@ -154,12 +154,12 @@ def prod(iterable, start=1):
 
 # Roughly equivalent to generic_bfs_edges from
 # https://networkx.github.io/documentation/stable/_modules/networkx/algorithms/traversal/breadth_first_search.html .
-def bfs_edges(source, neighbors):
+def bfs_edges(source, successors):
     visited = set([source])
     queue = deque([source])
     while queue:
         parent = queue.popleft()
-        for child in neighbors(parent):
+        for child in successors(parent):
             if child not in visited:
                 yield parent, child
                 visited.add(child)
