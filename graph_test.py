79d732f03863a50986a8a1fdce81cf6bbd54405c
Frederick Akalin <akalin@gmail.com>
2019-12-25 21:13:59 -0600

    got test to work with hack

--- graph_test.py
+++ graph_test.py
@@ -101,5 +101,15 @@ class TestGraph(unittest.TestCase):
             return astar(source, target, weighted_successors, zero_heuristic)
         self._test_path_length(path_length)
 
+    def test_astar_ideal_heuristic(self):
+        def path_length(source, target, weighted_successors, weighted_predecessors):
+            def ideal_heuristic(n):
+                try:
+                    return dijkstra_path_length(n, target, weighted_successors)
+                except ValueError:
+                    return 1000000
+            return astar(source, target, weighted_successors, ideal_heuristic)
+        self._test_path_length(path_length)
+
 if __name__ == '__main__':
     unittest.main()
