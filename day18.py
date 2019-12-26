e00314106246edbb9eaa4a22f2f46c0bff9892b4
Frederick Akalin <akalin@gmail.com>
2019-12-26 00:37:27 -0600

    got part 1 kinda working

--- day18.py
+++ day18.py
@@ -1,196 +1,108 @@
-from collections import deque
-from intcode import *
 from util import *
-from vec2 import *
+from heapq import heappush, heappop
+from itertools import count
+import networkx as nx
 
-def do_bfs(start, get_neighbor_fn, visit_fn):
-    visited = set([start])
-    queue = deque([start])
-    while queue:
-        n = queue.popleft()
-        for m in get_neighbor_fn(n):
-            if m not in visited:
-                visited.add(m)
-                visit_fn(m, n)
-                queue.append(m)
+def dijkstra_path_length_fn(source, is_target, weighted_successors):
+    dist = {}
+    seen = {source: 0}
+    # Use a counter to avoid comparing the nodes themselves in the
+    # heap.
+    c = count()
+    fringe = []
+    heappush(fringe, (0, next(c), source))
+    while fringe:
+        (d, _, v) = heappop(fringe)
+        if is_target(v):
+            return d
+        if v in dist:
+            continue # already searched this node
+        dist[v] = d
+        for u, cost in weighted_successors(v):
+            vu_dist = dist[v] + cost
+            if u in dist:
+                if vu_dist < dist[u]:
+                    raise ValueError(f'Contradictory paths found: negative weights? v={v} u={u} vu_dist={vu_dist} dist[u]={dist[u]}')
+            elif u not in seen or vu_dist < seen[u]:
+                seen[u] = vu_dist
+                heappush(fringe, (vu_dist, next(c), u))
 
-def do_a_star(start, get_neighbor_fn, h, is_goal):
-    open_set = set([start])
-    came_from = {start: None}
-
-    g_score = {start: 0}
-    f_score = {start: h(start)}
-
-    def get_g_score(n):
-        if n not in g_score:
-            return (1, 0)
-        return (0, g_score[n])
-
-    def get_f_score(n):
-        if n not in f_score:
-            return (1, 0)
-        return (0, f_score[n])
-
-    while len(open_set) > 0:
-        n = min(open_set, key=get_f_score)
-        if is_goal(n):
-            return g_score, f_score, came_from
-        open_set.remove(n)
-
-        for m, m_len in get_neighbor_fn(n):
-            if n not in g_score:
-                continue
-
-            tentative_g_score = g_score[n] + m_len
-            if (0, tentative_g_score) < get_g_score(m):
-                came_from[m] = n
-                g_score[m] = tentative_g_score
-                f_score[m] = g_score[m] + h(m)
-                open_set.add(m)
-
-    return None
+    raise ValueError(f'No path between {source} and {target}')
 
 def compute_day18(input):
+    input = '''
+#########
+#b.A.@.a#
+#########
+'''
     lines = [x.strip() for x in input.strip().split('\n')]
     rows = len(lines)
     cols = len(lines[0])
 
-    initial_pos = []
+    start_pos = None
     pos_to_key = {}
     key_to_pos = {}
     pos_to_door = {}
     door_to_pos = {}
-    walls = set()
+    walkables = set()
 
     for y in range(rows):
         for x in range(cols):
             c = lines[y][x]
-            p = Vec2(x, y)
+            if c == '#':
+                continue
+            p = (x, y)
             if c == '@':
-                initial_pos.append(p)
-            elif c == '#':
-                walls.add(p)
+                start_pos = p
+            elif c == '.':
+                pass
             elif 'a' <= c <= 'z':
                 pos_to_key[p] = c
                 key_to_pos[c] = p
             elif 'A' <= c <= 'Z':
                 pos_to_door[p] = c
                 door_to_pos[c] = p
+            else:
+                raise
 
-    if len(initial_pos) != 4:
+            walkables.add(p)
+
+    if start_pos is None:
         raise
 
-    initial_pos = tuple(initial_pos)
+    def local_neighbors(n, walkables):
+        return [m for m in dir_neighbors(n) if m in walkables]
 
-    def get_raw_dists(pos):
-        dists = {pos: 0}
-        blockers = {pos: frozenset()}
-        key_dists = {}
-        def get_neighbor_fn(n):
-            if n in walls:
-                return []
-            return [m for m in dir_neighbors(n) if (m not in walls)]
+    local = nx.Graph([(n, m) for n in walkables for m in local_neighbors(n, walkables)])
 
-        def visit_fn(n, parent):
-            dists[n] = dists[parent] + 1
-            new_blockers = blockers[parent]
-            if n in pos_to_door:
-                new_blockers |= frozenset(pos_to_door[n])
-            blockers[n] = new_blockers
+    G = nx.Graph()
 
-        do_bfs(pos, get_neighbor_fn, visit_fn)
+    labeled_nodes = [ start_pos ] + list(pos_to_key.keys())
+    for source in labeled_nodes:
+        local_lengths = nx.shortest_path_length(local, source)
+        for target in labeled_nodes:
+            if target != source and target in local_lengths:
+                G.add_edge(source, target, weight=local_lengths[target])
 
-        return {k: (dists[p], blockers[p]) for k, p in key_to_pos.items() if p in dists and p in blockers}
+    print(labeled_nodes, G.nodes, G.edges)
 
-    dist_cache = {}
-    def get_raw_dists_cached(pos):
-        if pos in dist_cache:
-            return dist_cache[pos]
-        v = get_raw_dists(pos)
-        dist_cache[pos] = v
-        return v
-
-    def get_dists(pos, inventory):
-        key_info = get_raw_dists_cached(pos)
-        v = {k: d for k, (d, b) in key_info.items() if inventory.issuperset([d.lower() for d in b]) and k not in inventory}
-        return v
-
-    def do_single(starting_pos, starting_inventory):
-        def get_neighbor_fn(n):
-            nonlocal max_l
-            pos, inventory = n
-            neighbors = []
-            key_dists = get_dists(pos, inventory)
-            for k, dist in key_dists.items():
-                new_pos = key_to_pos[k]
-                new_inventory = inventory | frozenset([k])
-                new_state = (new_pos, new_inventory)
-                neighbors.append((new_state, dist))
-            return neighbors
-
-        def heuristic(n):
-            pos, inventory = n
-            s = 0
-            key_dists = get_dists(pos, inventory)
-            if len(key_dists) > 0:
-                s = max(key_dists.values())
-            return s
-
-        dists = get_dists(starting_pos, starting_inventory)
-        all_keys = starting_inventory | frozenset(dists.keys())
-        def is_goal(n):
-            return n[1] == all_keys
-
-        initial_state = (starting_pos, starting_inventory)
-
-        dists, f_dists, prev = do_a_star(initial_state, get_neighbor_fn, heuristic, is_goal)
-        final_states = [(state[1], dist) for (state, dist) in dists.items() if frozenset(state[1]) == all_keys]
-        return min(final_states, key=lambda x: x[1])
-
-    initial_state = (initial_pos, frozenset())
-
-    max_l = 0
-    def get_neighbor_fn(n):
-        nonlocal max_l
-        positions, inventory = n
-        if len(inventory) > max_l:
-            print(max_l)
-            max_l = len(inventory)
-        neighbors = []
-        for i, pos in enumerate(positions):
-            key_dists = get_dists(pos, inventory)
-            for k, dist in key_dists.items():
-                new_positions = list(positions)
-                new_positions[i] = key_to_pos[k]
-                new_positions = tuple(new_positions)
-                new_inventory = inventory | frozenset([k])
-                new_state = (new_positions, new_inventory)
-                neighbors.append((new_state, dist))
-        return neighbors
-
-    def heuristic(n):
-        positions, inventory = n
-        s = 0
-        for pos in positions:
-            single_res = do_single(pos, inventory)
-            s += single_res[1]
-        return s
+    def weighted_neighbors(state):
+        pos, inventory = state
+        for new_pos, attributes in G[pos].items():
+            if new_pos == start_pos:
+                continue
+            new_inventory = inventory | frozenset([pos_to_key[new_pos]])
+            new_state = (new_pos, new_inventory)
+            yield (new_state, attributes['weight'])
 
     all_keys = frozenset(key_to_pos.keys())
-    def is_goal(n):
-        return n[1] == all_keys
+    def is_target(state):
+        return state[1] == all_keys
 
-    dists, f_dists, prev = do_a_star(initial_state, get_neighbor_fn, heuristic, is_goal)
-#    for d in dists.items():
-#        print(d)
-#    for p in prev.items():
-#        print(p)
+    start_state = (start_pos, frozenset())
+    part1 = dijkstra_path_length_fn(start_state, is_target, weighted_neighbors)
 
-#    do_bfs(initial_state, get_neighbor_fn, visit_fn)
-    final_states = [(state[1], dist) for (state, dist) in dists.items() if frozenset(state[1]) == all_keys]
-    print('min', min(final_states, key=lambda x: x[1]))
-
-    return None, None
+    return part1, None
 
 if __name__ == '__main__':
     with open('day18.input', 'r') as input_file:
